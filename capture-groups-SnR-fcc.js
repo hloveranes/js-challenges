e56dc9e91d5516419090862f514209a6820b68a7
loveranes <59357602+hloveranes@users.noreply.github.com>
2022-07-22 15:43:23 +0800

    Update capture-groups-SnR-fcc.js

--- capture-groups-SnR-fcc.js
+++ capture-groups-SnR-fcc.js
@@ -1,3 +1,7 @@
+// JavaScript Algorithms and Data Structures
+// Regular Expressions
+// Use Capture Groups to Search and Replace
+
 let str = "one two three";
 let fixRegex = /(\w+)(\s)(\w+)(\s)(\w+)/; // Change this line
 let replaceText = "$5 $3 $1"; // Change this line
