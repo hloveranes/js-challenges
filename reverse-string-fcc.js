372f497b4bca4ef8c9847349f61113eaff0643d1
loveranes <59357602+hloveranes@users.noreply.github.com>
2022-07-26 11:20:39 +0800

    Create reverse-string-fcc.js

---
+++ reverse-string-fcc.js
@@ -0,0 +1,9 @@
+// JavaScript Algorithms and Data Structures
+// Basic Algorithm Scripting
+// Reverse a String
+
+function reverseString(str) {
+  return str.split('').reverse().join('')
+}
+
+reverseString("hello");
