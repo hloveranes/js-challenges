995934583dd4e8b60d584f3fc640d97b8670ecb5
loveranes <59357602+hloveranes@users.noreply.github.com>
2022-07-26 16:07:47 +0800

    Update finders-keepers-fcc.js

--- finders-keepers-fcc.js
+++ finders-keepers-fcc.js
@@ -3,14 +3,12 @@
 // Finders Keepers
 
 function findElement(arr, func) {
-  let hasPassed = false;
   for (var i = 0; i < arr.length; i++ ){
      if(func(arr[i])) {
-       hasPassed = true;
       return arr[i];
      }
   } 
-  if(!hasPassed) return undefined;
+  return undefined;
 }
 
 findElement([1, 3, 5, 8, 9, 10], num => num % 2 === 0);
