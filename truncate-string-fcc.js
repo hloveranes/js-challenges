08bc496bcadeda88a5f6294a036aa957717e0bdb
loveranes <59357602+hloveranes@users.noreply.github.com>
2022-07-26 15:48:59 +0800

    Create truncate-string-fcc.js

---
+++ truncate-string-fcc.js
@@ -0,0 +1,9 @@
+// JavaScript Algorithms and Data Structures
+// Basic Algorithm Scripting
+// Truncate a String
+
+function truncateString(str, num) {
+  return str.length > num ? str.slice(0, num) + '...' : str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
